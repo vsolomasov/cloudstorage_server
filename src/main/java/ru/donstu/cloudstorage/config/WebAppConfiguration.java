db3296f139cad10893e27d6cc7048701513491c3
Соломасов Вячеслав <vyacheslafka@gmail.com>
2016-11-14 17:36:45 +0300

    Thymeleaf and static web resources configuration

--- src/main/java/ru/donstu/cloudstorage/config/WebAppConfiguration.java
+++ src/main/java/ru/donstu/cloudstorage/config/WebAppConfiguration.java
@@ -1,19 +1,68 @@
 package ru.donstu.cloudstorage.config;
 
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
-import static ru.donstu.cloudstorage.config.constant.Constants.PACKAGE_PROJECT;
+import static ru.donstu.cloudstorage.config.constant.Constants.*;
 
 /**
- * Конфигурация Web MVC
+ * Конфигурация Web, Thymeleaf
  *
  * @author v.solomasov
  */
 @Configuration
 @EnableWebMvc
 @ComponentScan(PACKAGE_PROJECT)
-public class WebAppConfiguration extends WebMvcConfigurerAdapter {
+public class WebAppConfiguration extends WebMvcConfigurerAdapter implements ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setApplicationContext(this.applicationContext);
+        templateResolver.setPrefix(PATH_TEMPLATES);
+        templateResolver.setSuffix(SUFFIX_TEMPLATES);
+        templateResolver.setTemplateMode(TemplateMode.HTML);
+        templateResolver.setCacheable(true);
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setEnableSpringELCompiler(true);
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafViewResolver viewResolver(){
+        ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+        viewResolver.setTemplateEngine(templateEngine());
+        return viewResolver;
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        super.addResourceHandlers(registry);
+        registry.addResourceHandler(CSS_HANDLER).addResourceLocations(CSS_LOCATION);
+        registry.addResourceHandler(JS_HANDLER).addResourceLocations(JS_LOCATION);
+    }
 }
--- src/main/java/ru/donstu/cloudstorage/config/constant/Constants.java
+++ src/main/java/ru/donstu/cloudstorage/config/constant/Constants.java
@@ -21,4 +21,14 @@ public class Constants {
     public final static String PACKAGE_CONFIG = PACKAGE_PROJECT + ".config";
 
     public final static String RESOURCES_PROPERTY = "classpath:application.properties";
+
+    public final static String PATH_TEMPLATES = "/templates/";
+
+    public final static String SUFFIX_TEMPLATES = ".html";
+
+    public final static String CSS_LOCATION = "/static/css/";
+    public final static String CSS_HANDLER = CSS_LOCATION + "**";
+
+    public final static String JS_LOCATION = "/static/js/";
+    public final static String JS_HANDLER = JS_LOCATION + "**";
 }
