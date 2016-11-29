87f4a74bcfb08538a851b1974d347c9a4aab7deb
Соломасов Вячеслав <vyacheslafka@gmail.com>
2016-11-29 19:33:37 +0300

    Fix static resources

--- src/main/java/ru/donstu/cloudstorage/web/exception/ExceptionHandlingController.java
+++ src/main/java/ru/donstu/cloudstorage/web/exception/ExceptionHandlingController.java
@@ -1,11 +1,13 @@
 package ru.donstu.cloudstorage.web.exception;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import ru.donstu.cloudstorage.service.security.SecurityService;
 
 import javax.servlet.http.HttpServletRequest;
 
@@ -21,6 +23,9 @@ public class ExceptionHandlingController {
 
     private static final String PAGE_NOT_FOUND = "Страницы (%s), не существует";
 
+    @Autowired
+    private SecurityService securityService;
+
     @ResponseStatus(HttpStatus.NOT_FOUND)
     @ExceptionHandler(NoHandlerFoundException.class)
     public ModelAndView handleError404(HttpServletRequest request, Exception ex) {
@@ -28,6 +33,7 @@ public class ExceptionHandlingController {
         String requestUri = request.getRequestURI();
         if (request != null) {
             modelAndView.addObject("error", String.format(PAGE_NOT_FOUND, requestUri));
+            modelAndView.addObject("isLogged", securityService.isLoggedUser());
         }
         return modelAndView;
     }
